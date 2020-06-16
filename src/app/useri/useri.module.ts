e8bc92fc13af69f1d820c8f1fe0ff9272270083a
tej-c <chettipallig.teja@st.niituniversity.in>
2020-06-17 00:48:06 +0530

    c

--- src/app/useri/useri.module.ts
+++ src/app/useri/useri.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeadComponent } from './head/head.component';
 import { LayComponent } from './lay/lay.component';
-import { FootComponent } from './foot/foot.component';
 import {MatMenuModule} from '@angular/material/menu';
 import { AppRoutingModule } from '../app-routing.module';
 import { ConComponent } from './con/con.component';
@@ -11,12 +10,12 @@ import { ConComponent } from './con/con.component';
 
 
 @NgModule({
-  declarations: [HeadComponent, LayComponent, FootComponent, ConComponent],
+  declarations: [HeadComponent, LayComponent, ConComponent],
   imports: [
     CommonModule,
     MatMenuModule,
     AppRoutingModule,
   ],
-  exports:[HeadComponent,LayComponent,FootComponent,ConComponent]
+  exports:[HeadComponent,LayComponent,ConComponent]
 })
 export class UseriModule { }

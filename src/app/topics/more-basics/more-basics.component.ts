e811ff023d9b2a3d67bc9095e7eeeacb585a873d
tej-c <57935598+tej-c@users.noreply.github.com>
2020-07-03 19:31:23 +0530

    new mod

---
+++ src/app/topics/more-basics/more-basics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-more-basics',
+  templateUrl: './more-basics.component.html',
+  styleUrls: ['./more-basics.component.css']
+})
+export class MoreBasicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

2189f057faf2b6adbce06fec5ed00c898ec7bbdc
tej-c <57935598+tej-c@users.noreply.github.com>
2020-07-04 09:43:55 +0530

    new mod

--- src/app/topics/topics.module.ts
+++ src/app/topics/topics.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicsComponent } from './basics/basics.component';
 import { MoreBasicsComponent } from './more-basics/more-basics.component';
+import { AdvanceComponent } from './advance/advance.component';
 
 
 
 @NgModule({
-  declarations: [BasicsComponent, MoreBasicsComponent],
+  declarations: [BasicsComponent, MoreBasicsComponent, AdvanceComponent],
   imports: [
     CommonModule
   ]
